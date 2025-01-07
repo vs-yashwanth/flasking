da18af82cbbc5ee1f8cf231c7633579b6b3b24e0
yashwanthvs <yashwanthvs@gmail.com>
2025-01-08 01:33:44 +0530

    feature/add-basic-endpoits

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 stores = [
     {
-        'name': 'My Wonderful Store',
+        'name': 'My Store',
         'items': [
             {
                 'name': 'Chair',
@@ -18,6 +18,40 @@ stores = [
 def home():
     return 'Hello Flask!'
 
-@app.get('/store')
+@app.get('/stores')
 def get_stores():
     return {'stores': stores}
+
+@app.post('/store')
+def create_store():
+    data = request.get_json()
+    new_store = {
+        'name': data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return new_store, 201
+
+@app.post('/store/<string:name>/item')
+def create_item(name):
+    data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            store['items'].append(data)
+            return store, 201
+    
+    return {'message':"Store not found"}, 400
+
+@app.get('/store/<string:name>')
+def get_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return store, 201
+    return {'message': 'Store not found'}, 404
+
+@app.get('/store/<string:name>/items')
+def get_items(name):
+    for store in stores:
+        if store['name'] == name:
+            return store['items'], 201
+    return {'message': 'Store not found'}, 404
